4a88487f76e4f976cc8b93468b98a64399d07ee7
boceto1 <janka.obando@outlook.es>
2019-11-12 21:22:39 -0500

    Blockchain class

---
+++ OwnBlockchain/blockchain.test.js
@@ -0,0 +1,24 @@
+const Blockchain = require('./blockchain');
+const Block = require('./block');
+
+
+describe('Blockchain', ()=>{
+    const blockchain = new Blockchain();
+
+    it('contains a `chain` Array instance', ()=> {
+
+    });
+
+    it('starts with the genesis block', ()=>{
+        expect(blockchain.chain[0]).toEqual(Block.genesis());
+    });
+
+    it('add a new block to the chain', ()=>{
+        const newData = 'foo bar';
+        blockchain.addBlock({data: newData});
+
+        expect(blockchain.chain[blockchain.chain.length-1].data).toEqual(newData);
+    });
+
+
+});
