68b07ace1d4f73e3e7c85c74582b97d0e0e88455
boceto1 <janka.obando@outlook.es>
2019-11-19 19:15:02 -0500

    blockchain network

---
+++ OwnBlockchain/const.example.js
@@ -0,0 +1,6 @@
+// Check app in https://www.pubnub.com/
+const CREDENTIALS = {
+    publishKey: 'foo-pub-key',
+    subscribeKey: 'foo-sub-key',
+    secretKey: 'foo-secret-key'
+}
---
+++ OwnBlockchain/pubsub.pubnub.js
@@ -0,0 +1,36 @@
+const PubNub = require('pubnub');
+
+const credentials = require('./const');
+
+const CHANNELS = {
+    TEST: 'TEST',
+    TESTTWO: 'TESTTWO'
+};
+
+class PubSub {
+    constructor(){
+        this.pubnub = new PubNub(credentials);
+        
+        this.pubnub.subscribe({channels: Object.values(CHANNELS)});
+        this.pubnub.addListener(this.listener());
+    }
+
+    listener(){
+        return {
+            message: messageObject =>{
+                const { channel, message} = messageObject;
+                console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+            }
+        };
+    }
+
+    publish({channel, message}){
+        this.pubnub.publish({channel,message});
+    }
+}
+
+const testPubSub = new PubSub();
+testPubSub.publish({channel: CHANNELS.TEST, message:'hello pubnub'})
+
+
+module.exports = PubSub;
