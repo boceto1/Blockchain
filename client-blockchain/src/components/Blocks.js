dd24c753b3e3b00ef09be438d89fa81959652083
boceto1 <janka.obando@outlook.es>
2020-01-14 18:24:49 -0500

    Bock component

--- client-blockchain/src/components/Blocks.js
+++ client-blockchain/src/components/Blocks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Block from './Block';
 
 class Blocks extends Component {
 
@@ -19,7 +20,10 @@ class Blocks extends Component {
                 {
                     this.state.blocks.map(block => {
                         return (
-                        <div key={block.hash} className='Block'>{block.hash}</div>
+                        <div>
+                            <div key={block.hash} className='Block'>{block.hash}</div>
+                            <Block key={block.hash} block={block}/>
+                        </div>
                         )
                     })
                 }
